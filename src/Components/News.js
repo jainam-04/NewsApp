d55137f45e2e8bc751a53f9936d4fa22022e1b67
Jainam <jainamrupani04@gmail.com>
2024-02-07 20:25:58 +0530

    Added news api and cards

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,8 +5,20 @@ export class News extends Component {
       render() {
             return (
                   <div>
-                        This is a news component.
-                        <NewsItem />
+                        <div className="container my-3">
+                              <h2>NewsMonkey - Top Headlines</h2>
+                              <div className="row">
+                                    <div className="col-md-4">
+                                          <NewsItem title="myTitle" description="myDescription"/>
+                                    </div>
+                                    <div className="col-md-4">
+                                          <NewsItem title="myTitle" description="myDescription"/>
+                                    </div>
+                                    <div className="col-md-4">
+                                          <NewsItem title="myTitle" description="myDescription"/>
+                                    </div>
+                              </div>
+                        </div>
                   </div>
             )
       }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
       render() {
+            let {title, description} = this.props;
             return (
                   <div>
-                        This is a news item component
+                        <div className="card" style={{width: "18rem"}}>
+                              <img src="https://www.aljazeera.com/wp-content/uploads/2024/02/Naman-Tiwari-of-India-in-bowling-action-during-the-ICC-U19-Mens-Cricket-World-Cup-South-Africa-2024-Semi-Final-match-between-India-and-South-Africa-at-Willowmoore-Park-on-February-06-2024-in-Benoni-South-Africa-1707209612.jpg?resize=1920%2C1440" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                          <h5 className="card-title">{title}</h5>
+                                          <p className="card-text">{description}</p>
+                                          <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                        </div>
                   </div>
             )
       }
