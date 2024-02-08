cdc4b0ac69c7c47e83ddc8be5f0d9af9592bec78
Jainam <jainamrupani04@gmail.com>
2024-02-08 19:01:16 +0530

    Added constructor and states

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
       render() {
-            let {title, description} = this.props;
+            let {title, description, imageUrl, newsUrl} = this.props;
             return (
-                  <div>
+                  <div className='my-3'>
                         <div className="card" style={{width: "18rem"}}>
-                              <img src="https://www.aljazeera.com/wp-content/uploads/2024/02/Naman-Tiwari-of-India-in-bowling-action-during-the-ICC-U19-Mens-Cricket-World-Cup-South-Africa-2024-Semi-Final-match-between-India-and-South-Africa-at-Willowmoore-Park-on-February-06-2024-in-Benoni-South-Africa-1707209612.jpg?resize=1920%2C1440" className="card-img-top" alt="..."/>
+                              <img src={imageUrl} alt="..."/>
                                     <div className="card-body">
                                           <h5 className="card-title">{title}</h5>
                                           <p className="card-text">{description}</p>
-                                          <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+                                          <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
                                     </div>
                         </div>
                   </div>
