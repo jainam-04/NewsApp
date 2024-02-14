b1915323bda2c22a016ab8600e7f3ee07e8687f5
Jainam <jainamrupani04@gmail.com>
2024-02-14 19:15:56 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
         <Router>
+          <NavBar/>
           <Routes>
             <Route
               path="/"
