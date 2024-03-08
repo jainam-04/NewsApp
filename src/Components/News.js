d1678edb61e5e02c74f05ce029cef5148db44e4a
Jainam <jainamrupani04@gmail.com>
2024-03-08 15:35:02 +0530

    Spinner is removed

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-import Spinner from "./Spinner";
 import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -49,7 +48,7 @@ export class News extends Component {
 
       fetchMoreData = async () => {
             this.setState({ page: this.state.page + 1 });
-            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ad45d68901394d7eab1278d39aa86d71&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ad45d68901394d7eab1278d39aa86d71&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             let data = await fetch(url);
             let parsedData = await data.json();
             this.setState({
@@ -63,12 +62,10 @@ export class News extends Component {
                   <div>
                         <>
                               <h1 className="text-center" style={{ margin: "2vw 0" }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-                              {this.state.loading && <Spinner />}
                               <InfiniteScroll
                                     dataLength={this.state.articles.length}
                                     next={this.fetchMoreData}
                                     hasMore={this.state.articles.length !== this.state.totalResults}
-                                    loader={<Spinner />}
                               >
                                     <div className="container">
                                           <div className="row">
@@ -77,9 +74,7 @@ export class News extends Component {
                                                             <div className="col-md-4" key={element.url}>
                                                                   <NewsItem
                                                                         title={element.title ? element.title : ""}
-                                                                        description={
-                                                                              element.description ? element.description : ""
-                                                                        }
+                                                                        description={element.description ? element.description : ""}
                                                                         imageUrl={element.urlToImage}
                                                                         newsUrl={element.url}
                                                                         author={element.author}
@@ -89,6 +84,7 @@ export class News extends Component {
                                                             </div>
                                                       );
                                                 })}
+
                                           </div>
                                     </div>
                               </InfiniteScroll>
